6a5788838f64356080f57247a3fe437c12e38eb3
Ehllay <max.pruther@gmail.com>
2024-05-23 15:44:25 +0100

    Added toggle and default window size

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
+use std::cell::Cell;
+
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{glib, Align, Application, ApplicationWindow, Button};
 
 const APP_ID: &str = "org.gtk_rs.TimeZZ";
 
 fn build_ui(app: &Application) {
     // Buttons
-    let button = Button::builder()
+    let start_button = Button::builder()
         .label("Start the timer!")
         .margin_top(12)
         .margin_bottom(12)
+        .halign(Align::Center)
+        .valign(Align::End)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    button.connect_clicked(|button| {
-        button.set_label("Stop the timer");
+    let paused = Cell::new(false);
+
+    start_button.connect_clicked(move |button| {
+        if !paused.get() {
+            button.set_label("Stop the timer");
+            paused.set(true);
+        } else {
+            button.set_label("Start the timer!");
+            paused.set(false);
+        };
     });
 
     // Create window
     let window = ApplicationWindow::builder()
         .application(app)
+        .height_request(300)
+        .width_request(250)
+        .default_width(250)
+        .default_height(300)
         .title("TimeZZ")
-        .child(&button)
+        .child(&start_button)
         .build();
     window.present()
 }
